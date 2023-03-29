b821438113eb86c414aa1adb7f35ed5cc050ea3b
Perkles <juliobh87@gmail.com>
2023-03-28 22:56:20 -0300

    alterado num3 = 40

--- src/academy/devdojo/maratonajava/MediaTresNumeros.java
+++ src/academy/devdojo/maratonajava/MediaTresNumeros.java
@@ -3,7 +3,7 @@ public class MediaTresNumeros {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 20;
-        int num3 = 30;
+        int num3 = 40;
         double media = (num1 + num2 + num3) / 3.0;
         System.out.println("A média de " + num1 + ", " + num2 + " e " + num3 + " é: " + media);
     }
